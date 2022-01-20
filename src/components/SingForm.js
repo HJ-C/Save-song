a2c11905fbebcb8c9746dbb1286e842be7874ebc
HJ-C <swch56naver.com>
2022-01-20 10:31:23 +0900

    Error msg 수정

--- src/components/SingForm.js
+++ src/components/SingForm.js
@@ -5,20 +5,45 @@ const SingForm = ({addSing}) => {
     
   const [singTitle, setSingTitle] = useState('')
   const [singYear, setSingYear] = useState('')
+  const [titleError, setTitleError] = useState('')
+  const [yearError, setYearError] = useState('')
     
     const resetForm = ()=>{
         setSingTitle('')
         setSingYear('')
     }
 
+    const validateForm = ()=> {
+      let validated = true
+
+      if (!singTitle){
+        setTitleError('노래제목을 넣어주세요')
+        validated = false
+      }
+      if (!singYear){
+        setYearError('개봉년도를 넣어주세요')
+        validated = false
+      }
+      return validated
+    }
+
+    const resetErrors = ()=>{
+      setTitleError('')
+      setYearError('')
+    }
+
     const onSubmit = (e)=> {
       e.preventDefault()
-      addSing( {
-        id : Date.now(),
-        title : singTitle,
-        year : singYear,
-      })
-      resetForm()
+      if(validateForm()){
+        addSing( {
+          id : Date.now(),
+          title : singTitle,
+          year : singYear,
+        })
+        resetErrors()
+        resetForm()
+      } 
+
     }
   
     return (
@@ -29,13 +54,19 @@ const SingForm = ({addSing}) => {
           placeholder='노래제목'
           onChange={e => setSingTitle(e.target.value)}></input>
           <br></br>
+
+          <div style={{color:'red'}}>{titleError}</div>
+
           <input
-          type = "text"
+          type = "number"
           value={singYear}
           placeholder='개봉년도'
           onChange={e => setSingYear(e.target.value)}></input>
           <br></br>
           <button type = "submit">노래 추가</button>
+
+          <div style={{color:'red'}}>{yearError}</div>
+
         </form>
     )
 }
